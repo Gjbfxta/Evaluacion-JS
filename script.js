6c05b8e838e76160833ab8a2777016c2a7971a47
Takodepoio <163039765+Takodepoio@users.noreply.github.com>
2024-10-07 13:35:52 -0600

    listo mi ejercicio

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let nombre = prompt ("Escribe tu nombre");
+console.log(nombre)
+
+let edad = prompt ("Indica tu edad");
+console.log(edad);
+
+let estudiaste = true 
+
+let saludo = `¡Hola! Soy  ${nombre} y tengo ${edad} años 🏄‍♀️`;
+  console.log (saludo);
+
+  let si = `¡${nombre}! ¡Si estudiaste! pasaras tu examen`;
+
+  let no = `¡${nombre}! ¡No estudiaste! Nos vemos en extra`;
+  if (estudiaste> false )
+    {
+    alert (no);
+  } else {
+    alert (false);
+  }
+
